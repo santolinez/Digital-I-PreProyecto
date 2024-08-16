7bb51254e2fa3561f4c2ef36e1cd9faf537ffa6c
NataliaDuenas <104275525+NataliaDuenas@users.noreply.github.com>
2024-08-16 18:51:12 -0500

    Ultrasonido con maquina de estado bien hecha xd

--- codigos/Ultrasonido/Trigger.v
+++ codigos/Ultrasonido/Trigger.v
@@ -1,18 +1,17 @@
 module Trigger#(parameter COUNT_MAX = 25)(
     input clk,     // Señal de reloj
-    input Enable,  // Habilitación del módulo
     input Echo,
+    input rst,
     output Led,
-    output Trigger, // Salida del pulso de trigger
-    output reg Done // Indicador de finalización
+    output reg Trigger // Salida del pulso de trigge
    
 );
 
-reg [14:0] counter; // Contador de 15 bits
+reg [5:0] counter; // Contador de 15 bits
 reg [14:0] Tiempo;
 wire wait_echo_reg;
 reg trigger_done;
-reg [1:0] state;
+reg [1:0] fsm_state, next_state;
 reg [3:0] counter_10;
 reg led_reg;
 
@@ -25,14 +24,14 @@ parameter WAITECHO=3;
 
 
 initial begin
-    Done <= 0;
-    counter <= 0;    
-    Tiempo <= 0;  
-    state <= IDLE;  
+    counter <= 6'b000000;    
+    Tiempo <= 0;
+    Trigger <=0;  
+    fsm_state  <= IDLE;  
     trigger_done <= 0;
     micro <= 0;
     counter_10 <=0;
-	 led_reg <= 0;
+	led_reg <= 0;
     end
 
 // Divisor de frecuecia
@@ -41,34 +40,51 @@ always@(posedge clk) begin
             micro = ~micro;
             counter<=0;
         end else begin 
-            counter = counter +1;
+            counter <= counter + 6'b000001;
         end
     end
- 
-always@(posedge clk)begin
 
-case(state)
-IDLE: begin 
-    state=(Enable)?TRIGGER:state;
+always @(posedge micro)begin
+    if(rst==0) begin 
+        fsm_state <= IDLE;
+    end else begin 
+        fsm_state <= next_state;
+    end
 end
-TRIGGER: begin
-    state=(trigger_done)?WAIT:state;
-end
-WAIT: begin
-    state=(Echo)?WAITECHO:state;
-end
-WAITECHO: begin
-    state=(Echo)?state:IDLE;
-end
+
+ 
+always@(*)begin
+
+case(fsm_state)
+IDLE:
+    next_state=TRIGGER;
+TRIGGER:
+    next_state=(trigger_done)?WAIT:TRIGGER;
+WAIT:
+    next_state=(Echo)?WAITECHO:WAIT;
+WAITECHO: 
+    next_state=(Echo)?WAITECHO:IDLE;
 endcase
 end
 
-
-assign Trigger = (state==TRIGGER);
-assign wait_echo_reg = (state==WAITECHO);
-
-always @(posedge micro) begin
-    if (Trigger) begin
+always @(posedge micro) begin 
+    if (rst==0) begin
+        counter_10 <= 0;
+        Trigger <=0;
+        trigger_done <= 0;
+        Tiempo <=0;
+        led_reg = 0;
+    end else begin
+        case(next_state)
+        IDLE: begin
+        Trigger <=0; 
+        counter_10 <= 0;
+        trigger_done <= 0;
+        Tiempo <=0;
+        led_reg = 0;
+        end
+        TRIGGER: begin 
+        Trigger <=1;
         counter_10 <= counter_10 + 1;
         if (counter_10 < 10) begin // Si el contador es menor que 10, se activa el trigger
 			trigger_done <= 0;
@@ -76,29 +92,24 @@ always @(posedge micro) begin
             trigger_done <= 1; // Reinicia el contador 
             counter_10 <= 0;
         end
-    end else begin
-        trigger_done <= 0;
-    end
-end 
-
-
-always @(posedge micro) begin
-    if (wait_echo_reg)begin 
-    Done=0;
-    Tiempo <= Tiempo + 1;
-    end else begin
-    Done=1;
-    Tiempo <= 0;
-    end 
-end
-
-always @(posedge micro) begin
-    if (Tiempo < 1000) begin
+        end
+        WAIT: begin 
+        Trigger <=0;
+        
+        end
+        WAITECHO: begin 
+        Tiempo <= Tiempo + 1;
+        if (Tiempo < 1000) begin
         led_reg = 1;  // Enciende el LED si el tiempo es menor que 583
     end else begin
         led_reg = 0;  // Apaga el LED si el tiempo es mayor o igual a 583
     end 
+        end
+
+    endcase
+    end
 end
+
 
 assign Led = ~led_reg;
 
