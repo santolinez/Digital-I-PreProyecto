57e75ca861b687ed3045c974c7e8a8f52a2eb7ab
NataliaDuenas <104275525+NataliaDuenas@users.noreply.github.com>
2024-09-02 08:17:26 -0500

    Pollito con toda la maquina de estados(Intento)

--- codigos/Codigos_ILI/ili9341_top.v
+++ codigos/Codigos_ILI/ili9341_top.v
@@ -13,8 +13,8 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
 
     wire clk_out;
     wire clk_input_data;
-    reg [2:0] prev_visua;
-    reg [2:0] fsm_state, next_state, escalamiento;
+    reg [3:0] prev_visua;
+    reg [3:0] fsm_state, next_state, escalamiento;
     reg [PIXEL_SIZE-1:0] imagen;
     reg [PIXEL_SIZE-1:0] current_pixel;
     reg [PIXEL_SIZE-1:0] pixel_data_mem[0:RESOLUTION-1];
@@ -25,10 +25,19 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
     reg [1:0]counter_horizontal,counter_vertical;
 
     localparam IDLE = 0;
-    localparam TRISTE = 1;
-    localparam CARINO = 2;
-    localparam DEPRIMIDO = 3;
-    localparam MUERTO = 4;
+    localparam HAMBRE=1;
+    localparam DESNUTRIDO=2;
+    localparam COMIENDO=3;
+    localparam TOS=4;
+    localparam FIEBRE=5;
+    localparam PILDORA=6;
+    localparam CANSADO=7;
+    localparam DESVELO=8;
+    localparam DORMIDO=9;
+    localparam TRISTE= 10;
+    localparam DEPRESION= 11;
+    localparam CARISIA= 12;
+    localparam MUERTO= 13;
 
     initial begin 
         imagen <= 'h07FF;
@@ -62,10 +71,19 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
     always @(*) begin
         case(visua)
             0: next_state = IDLE;
-            1: next_state = TRISTE;
-            2: next_state = CARINO;
-            3: next_state = DEPRIMIDO;
-            4: next_state = MUERTO;
+            1: next_state = HAMBRE;
+            2: next_state = DESNUTRIDO;
+            3: next_state = COMIENDO;
+            4: next_state = TOS;
+            5: next_state = FIEBRE;
+            6: next_state = PILDORA;
+            7: next_state = CANSADO;
+            8: next_state = DESVELO;
+            9: next_state = DORMIDO;
+            10: next_state = TRISTE;
+            11: next_state = DEPRESION;
+            12: next_state = CARISIA;
+            13: next_state = MUERTO;
             default: next_state = IDLE;
         endcase
     end
@@ -93,6 +111,10 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
                             begin 
                                 escalamiento<= 'd1;
                             end
+                            if(transmission_done)
+                            begin 
+                                escalamiento<= 'd4;
+                            end
                         end
                         'd1: begin
                             pixel_memoria<=pixel_memoria+'b1;
@@ -107,23 +129,49 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
                                 counter_horizontal <= 'b0;
                                 escalamiento<= 'd0;
                             end
+                            if(transmission_done)
+                            begin 
+                                escalamiento<= 'd4;
+                            end
                         end
                         'd2: begin
                             pixel_memoria<=pixel_memoria-'d80;
                             counter_horizontal <= 'b0;
                             escalamiento<= 'd0;
+                            if(transmission_done)
+                            begin 
+                                escalamiento<= 'd4;
+                            end
                         end
                         'd3: begin
                             pixel_memoria<=pixel_memoria+'b1;
                             counter_vertical<=0;
                             counter_horizontal <= 'b0;
                             escalamiento<= 'd0;
+                            if(transmission_done)
+                            begin 
+                                escalamiento<= 'd4;
+                            end
+                        end
+                        'd4: begin
+                            counter_vertical<= 'b0;
+                            counter_horizontal <= 'b0;
+                            pixel_memoria<= 'b0;
                         end
                     endcase    
                 end
-                TRISTE: imagen <= 'h07FF; // Azul clarito
-                CARINO: imagen <= 'hF800; // Rojo
-                DEPRIMIDO: imagen <= 'h780F; // Morado
+                HAMBRE: imagen <= 'h07FF; // Azul clarito
+                DESNUTRIDO: imagen <= 'hF800; // Rojo
+                COMIENDO: imagen <= 'h780F; // Morado
+                TOS: imagen <= 'h0000; // Negro
+                FIEBRE: imagen <= 'h07FF; // Azul clarito
+                PILDORA: imagen <= 'hF800; // Rojo
+                CANSADO: imagen <= 'h780F; // Morado
+                DESVELO: imagen <= 'h0000; // Negro
+                DORMIDO: imagen <= 'h07FF; // Azul clarito
+                TRISTE: imagen <= 'hF800; // Rojo
+                DEPRESION: imagen <= 'h780F; // Morado
+                CARISIA: imagen <= 'h780F; // Morado
                 MUERTO: imagen <= 'h0000; // Negro
                 default: imagen <= 'h001F; // Azul oscuro
             endcase
