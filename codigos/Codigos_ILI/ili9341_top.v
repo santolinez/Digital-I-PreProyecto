a0f9a98254ef6ebc46db2b87cbf4eb039e604f50
NataliaDuenas <104275525+NataliaDuenas@users.noreply.github.com>
2024-09-02 09:43:20 -0500

    Pollo se supone que deberia cambiar

--- codigos/Codigos_ILI/ili9341_top.v
+++ codigos/Codigos_ILI/ili9341_top.v
@@ -25,19 +25,10 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
     reg [1:0]counter_horizontal,counter_vertical;
 
     localparam IDLE = 0;
-    localparam HAMBRE=1;
-    localparam DESNUTRIDO=2;
-    localparam COMIENDO=3;
-    localparam TOS=4;
-    localparam FIEBRE=5;
-    localparam PILDORA=6;
-    localparam CANSADO=7;
-    localparam DESVELO=8;
-    localparam DORMIDO=9;
-    localparam TRISTE= 10;
-    localparam DEPRESION= 11;
-    localparam CARISIA= 12;
-    localparam MUERTO= 13;
+    localparam TRISTE = 1;
+    localparam CARINO = 2;
+    localparam DEPRIMIDO = 3;
+    localparam MUERTO = 4;
 
     initial begin 
         imagen <= 'h07FF;
@@ -71,19 +62,10 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
     always @(*) begin
         case(visua)
             0: next_state = IDLE;
-            1: next_state = HAMBRE;
-            2: next_state = DESNUTRIDO;
-            3: next_state = COMIENDO;
-            4: next_state = TOS;
-            5: next_state = FIEBRE;
-            6: next_state = PILDORA;
-            7: next_state = CANSADO;
-            8: next_state = DESVELO;
-            9: next_state = DORMIDO;
-            10: next_state = TRISTE;
-            11: next_state = DEPRESION;
-            12: next_state = CARISIA;
-            13: next_state = MUERTO;
+            1: next_state = TRISTE;
+            2: next_state = CARINO;
+            3: next_state = DEPRIMIDO;
+            4: next_state = MUERTO;
             default: next_state = IDLE;
         endcase
     end
@@ -103,75 +85,53 @@ module ili9341_top #(parameter RESOLUTION = 240*240, parameter PIXEL_SIZE = 16, 
             prev_visua <= visua;
             case(fsm_state)
                 IDLE: begin
+                    escalamiento <='d0;
                         case(escalamiento)
-                        'd0: begin
+                        'd0:begin
+                            counter_horizontal <= 'b0;
+                            counter_vertical <= 'b0;
+                            pixel_memoria <= 'b0;
+                            escalamiento<= 'd1;
+                        end
+                        'd1: begin
                             imagen <= pixel_data_mem[pixel_memoria];
                             counter_horizontal <= counter_horizontal+1;
                             if(counter_horizontal==1)
                             begin 
-                                escalamiento<= 'd1;
-                            end
-                            if(transmission_done)
-                            begin 
-                                escalamiento<= 'd4;
+                                escalamiento<= 'd2;
                             end
                         end
-                        'd1: begin
+                        'd2: begin
                             pixel_memoria<=pixel_memoria+'b1;
                             if(pixel_memoria % 'd80==0 && pixel_memoria!='b0)begin
                                 counter_vertical<=counter_vertical+'b1;
                                 if(counter_vertical==2)begin 
-                                    escalamiento<= 'd3; 
+                                    escalamiento<= 'd4; 
                                 end else begin 
-                                    escalamiento<= 'd2; 
+                                    escalamiento<= 'd3; 
                                 end
                             end else begin
                                 counter_horizontal <= 'b0;
-                                escalamiento<= 'd0;
-                            end
-                            if(transmission_done)
-                            begin 
-                                escalamiento<= 'd4;
-                            end
-                        end
-                        'd2: begin
-                            pixel_memoria<=pixel_memoria-'d80;
-                            counter_horizontal <= 'b0;
-                            escalamiento<= 'd0;
-                            if(transmission_done)
-                            begin 
-                                escalamiento<= 'd4;
+                                escalamiento<= 'd1;
                             end
                         end
                         'd3: begin
+                            pixel_memoria<=pixel_memoria-'d80;
+                            counter_horizontal <= 'b0;
+                            escalamiento<= 'd1;
+                        end
+                        'd4: begin
                             pixel_memoria<=pixel_memoria+'b1;
                             counter_vertical<=0;
                             counter_horizontal <= 'b0;
-                            escalamiento<= 'd0;
-                            if(transmission_done)
-                            begin 
-                                escalamiento<= 'd4;
-                            end
+                            escalamiento<= 'd1;
                         end
-                        'd4: begin
-                            counter_vertical<= 'b0;
-                            counter_horizontal <= 'b0;
-                            pixel_memoria<= 'b0;
-                        end
+
                     endcase    
                 end
-                HAMBRE: imagen <= 'h07FF; // Azul clarito
-                DESNUTRIDO: imagen <= 'hF800; // Rojo
-                COMIENDO: imagen <= 'h780F; // Morado
-                TOS: imagen <= 'h0000; // Negro
-                FIEBRE: imagen <= 'h07FF; // Azul clarito
-                PILDORA: imagen <= 'hF800; // Rojo
-                CANSADO: imagen <= 'h780F; // Morado
-                DESVELO: imagen <= 'h0000; // Negro
-                DORMIDO: imagen <= 'h07FF; // Azul clarito
-                TRISTE: imagen <= 'hF800; // Rojo
-                DEPRESION: imagen <= 'h780F; // Morado
-                CARISIA: imagen <= 'h780F; // Morado
+                TRISTE: imagen <= 'h07FF; // Azul clarito
+                CARINO: imagen <= 'hF800; // Rojo
+                DEPRIMIDO: imagen <= 'h780F; // Morado
                 MUERTO: imagen <= 'h0000; // Negro
                 default: imagen <= 'h001F; // Azul oscuro
             endcase
